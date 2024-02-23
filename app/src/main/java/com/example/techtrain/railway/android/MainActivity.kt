c9470063f80366f37435215c373a87381a59cd8c
atsus <b1020028@fun.ac.jp>
2024-02-23 13:32:48 +0900

    記録用

--- app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
+++ app/src/main/java/com/example/techtrain/railway/android/MainActivity.kt
@@ -1,6 +1,10 @@
 package com.example.techtrain.railway.android
 
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
@@ -9,6 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<TextView>(R.id.text).text = "Hello Railway!"
+        //TextViewの設定
+        val textView = findViewById<TextView>(R.id.text)
+
+        //EditTextの設定
+        val editText = findViewById<EditText>(R.id.editText)
+
+        //Buttonの設定
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            textView.text = editText.text
+        }
     }
 }
