c9d7cb7806291e24002dcce4e690f52d19759abf
ats <atsushi.k214@gmail.com>
2024-03-16 18:12:11 +0900

    add:BookReviewActivityにツールバーを押すとログアウトできる処理

--- app/src/main/java/com/example/techtrain/railway/android/BookReviewActivity.kt
+++ app/src/main/java/com/example/techtrain/railway/android/BookReviewActivity.kt
@@ -1,6 +1,10 @@
 package com.example.techtrain.railway.android
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import com.example.techtrain.railway.android.databinding.ActivityBookreviewBinding
 
@@ -13,5 +17,30 @@ class BookReviewActivity: AppCompatActivity(){
         binding = ActivityBookreviewBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // ログアウトボタンを表示する
+        menuInflater.inflate(R.menu.menu_logout, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // ログアウトボタンが押された時の処理
+        return when (item.itemId) {
+            // トークンを削除
+            R.id.action_logout -> {
+                val dataStore = getSharedPreferences(getString(R.string.preference), Context.MODE_PRIVATE)
+                val editor = dataStore.edit()
+                editor.clear()
+                editor.apply()
+
+                // MainActivityに遷移する
+                val intent = Intent(this@BookReviewActivity, MainActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> false
+        }
     }
 }
